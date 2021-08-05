804b5e613bf06eea6ee987925f8e51d383b3bef1
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-08-05 11:46:10 -0400

    Updates

--- src/component/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import Navbar from './Navbar';
-
-describe('test for the application Navbar', () => {
-   it('should render without error', () => {
-      const wrapper = shallow(<Navbar />);
-      expect(wrapper).toHaveLength(1);
-   })
-})
